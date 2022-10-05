6fb009b7be9eabe0b9c0787ff24c1c84e9986a74
hermanbayron <foforito2001gg@gmail.com>
2022-10-05 18:35:03 +0000

    Semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
-const Home = () => {
+const Home = () => { 
+	
+	const [ colorRojo, setColor1] = useState("sombra");
+	const [ colorAmarillo, setColor2] = useState("sombra");
+	const [ colorVerde, setColor3] = useState("sombra");
+
+	function TrafficLight1() {
+
+		if(colorRojo === "sombra"){
+			setColor1("luz")
+			setColor2("sombra")
+			setColor3("sombra")
+		};
+	};
+
+		function TrafficLight2() {
+
+		if(colorAmarillo === "sombra"){
+			setColor2("luz")
+			setColor1("sombra")
+			setColor3("sombra")
+		};
+	};
+
+		function TrafficLight3() {
+
+		if(colorVerde === "sombra"){
+			setColor3("luz")
+			setColor1("sombra")
+			setColor2("sombra")
+		};
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center bg-dark m-3 d-flex justify-content-center rounded div">
+			<div>
+				<div className={"d-flex justify-content-center rounded-circle w-25 p-5 m-3 bg-danger rojo "+colorRojo} onClick={()=>TrafficLight1()}></div>
+				<div className={"d-flex justify-content-center rounded-circle w-25 p-5 m-3 bg-warning amarillo "+colorAmarillo} onClick={()=>TrafficLight2()}></div>
+				<div className={"d-flex justify-content-center rounded-circle w-25 p-5 m-3 bg-success verde "+colorVerde} onClick={()=>TrafficLight3()}></div>
+			</div>
 		</div>
 	);
 };
